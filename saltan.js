892d9d8e85cc6f3775d0bf2ebfdab99e5635b839
websalt <57046569+websalt@users.noreply.github.com>
2021-08-13 22:43:32 -0400

    Improve the performance

--- saltan.js
+++ saltan.js
@@ -10,13 +10,13 @@ function metalStatus(element) {
 }
 
 async function fetchData() {
-    let elements = fetch(
+    const elements = fetch(
         'https://api.github.com/repos/Bowserinator/Periodic-Table-JSON/contents/PeriodicTableJSON.json'
     ).then(function(data) {return data.json()}).then(
         function(data) {
             return JSON.parse(atob(data['content']))['elements']});
 
-    let polyatomic_ions = fetch(
+    const polyatomic_ions = fetch(
         'https://api.github.com/repos/websalt/saltan/contents/polyatomic_ions.json'
     ).then(function(data) {return data.json()}).then(
         function(data) {
@@ -35,7 +35,7 @@ function print(content) {
 }
 
 function prettyPresent(node) {
-    let prettified = node['symbol'] + ' ' + node['name'];
+    const prettified = node['symbol'] + ' ' + node['name'];
     if (node['number']) {
         return prettified + ' '+ node['number'];
     } else {
@@ -45,58 +45,91 @@ function prettyPresent(node) {
 
 
 function categorize(data) {
-    var categorized = {metals: [], nonmetals: []};
+    var categorized = {metals: {}, nonmetals: {}};
 
     for (i = 0; i < data['elements'].length; i++)  {
         let element = data['elements'][i];
         let elem_status = metalStatus(element);
         if (elem_status) {
-            categorized.metals.push(element);
+            categorized.metals[element.name] = element;
         } else if (elem_status === false) {
-            categorized.nonmetals.push(element);
+            categorized.nonmetals[element.name] = element;
         }
     }
 
     for (i = 0; i < data['polyatomic_ions'].length; i++) {
         let ion = data['polyatomic_ions'][i];
         if (ion['charge'] > 0) {
-            categorized.metals.push(ion);
+            categorized.metals[ion.name] = ion;
         } else {
-            categorized.nonmetals.push(ion);
+            categorized.nonmetals[ion.name] = ion;
         }
     }
     return categorized;
 }
 
-function sortString(str) {
-    return str.split('').sort((a, b) => a.localeCompare(b)).join('');
+function countChars(str) {
+    var charcounts = {};
+    for (i = 0; i < str.length; i++) {
+        let c = str[i]
+        charcounts[c] ? charcounts[c] += 1 : charcounts[c] = 1;
+    }
+    return charcounts;
 }
 
-function main(data, phrase) {
+function isAnagram(cc1, cc2) {
+  cc1Keys = Object.keys(cc1);
+  cc2Keys = Object.keys(cc2);
+  if (cc1Keys.length != cc2Keys.length) {
+    return false;
+  }
+  for (i = 0; i < cc1Keys.length; i++) {
+    let key = cc1Keys[i]
+    if (cc1[key] != cc2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function prepare(categorized) {
+    return Object.values(categorized.metals).map(function(metal) {
+        return Object.values(categorized.nonmetals).map(function(nonmetal) {
+            const charcounts = countChars(getSymbol(metal) + getSymbol(nonmetal))
+            return {
+                charcounts: charcounts,
+                metal: metal.name,
+                nonmetal: nonmetal.name
+            }
+        })
+    }).flat();
+}
+
+function saltAnagrams(cc, prepared) {
+    return prepared.filter(function(salt) {
+        return isAnagram(cc, salt.charcounts);
+    });
+}
+
+function main(categorized, prepared, phrase) {
     if (!phrase) {
         phrase = (window.location.hash.slice(1) || 'Salt');
     }
 
-    let sorted_phrase = sortString(phrase.toLowerCase().replace(/[^a-z]/g, ''))
-    let categorized = categorize(data);
+    const reduced = phrase.toLowerCase().replace(/[^a-z]/g, '')
 
     var content = phrase + "\n";
     var has_salt = false;
-    for (x = 0; x < categorized.metals.length; x++) {
-        let metal = categorized.metals[x];
-        for (y = 0; y < categorized.nonmetals.length; y++) {
-            let nonmetal = categorized.nonmetals[y];
-            let combo = sortString(getSymbol(metal) + getSymbol(nonmetal));
-            if (combo == sorted_phrase) {
-                if (has_salt) {
-                    content += "\n"
-                }
-                content += prettyPresent(metal) + "\n";
-                content += prettyPresent(nonmetal) + "\n";
-                has_salt = true;
-            }
+
+    saltAnagrams(countChars(reduced), prepared).forEach(function(salt) {
+        if (has_salt) {
+            content += '\n'
         }
-    }
+        content += prettyPresent(categorized.metals[salt.metal]) + '\n';
+        content += prettyPresent(categorized.nonmetals[salt.nonmetal]) + '\n'
+        has_salt = true
+    })
+
     if (!has_salt) {
         content += "No salts were found";
     }
@@ -105,21 +138,32 @@ function main(data, phrase) {
 
 function keyHandler(k) {
     if (k.key == 'Enter') {
-        let input = document.getElementById('phrase');
-        main(JSON.parse(localStorage.getItem('data')), input.value);
-        input.value = "";
+        var input = document.getElementById('phrase');
+        main(categorized, prepared, input.value);
+        input.value = '';
     }
 }
 
 async function onLoad() {
-    var data = localStorage.getItem('data');
-    if (data) {
-        data = JSON.parse(data);
+    window.categorized = localStorage.getItem('categorized');
+    window.prepared = localStorage.getItem('prepared');
+
+    if (window.categorized) {
+        window.categorized = JSON.parse(window.categorized);
     } else {
-        data = await fetchData();
-        localStorage.setItem('data', JSON.stringify(data));
+        console.log('Categorizing...');
+        window.categorized = categorize(await fetchData());
+        localStorage.setItem('categorized', JSON.stringify(window.categorized));
+    }
+
+    if (window.prepared) {
+        window.prepared = JSON.parse(window.prepared);
+    } else {
+        console.log('Preparing...');
+        window.prepared = prepare(categorized);
+        localStorage.setItem('prepared', JSON.stringify(window.prepared));
     }
     document.getElementById('phrase').addEventListener('keypress',
                                                        keyHandler);
-    main(data);
+    main(categorized, prepared);
 }
